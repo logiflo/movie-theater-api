ea7cadc6452314fadd777eae016536dc8b695ad7
Lorena Gil <lorenagil@fb.com>
2022-11-10 14:41:37 +0000

    Lint files.

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,12 @@
 // imports
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
 
 //create an instance of the database call it db
-const db = new Sequelize('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './movie_watchlist.sqlite',
-    logging: false
-})
-
-
+const db = new Sequelize("database", "username", "password", {
+  dialect: "sqlite",
+  storage: "./movie_watchlist.sqlite",
+  logging: false,
+});
 
 //export
-module.exports = { db, DataTypes }
+module.exports = { db, DataTypes };
