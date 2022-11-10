0ee3e6ac684835a16ece159397bc35cfaba73292
Lorena Gil <lorenagil@fb.com>
2022-11-10 15:50:44 +0000

    Added PUT in the userRouter

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { User } = require("../models");
+const { User, Show } = require("../models");
 
 const { getUserById } = require("../middleware");
 
@@ -18,5 +18,13 @@ userRouter.get("/:user_id/shows", getUserById, async (request, response) => {
   const userShows = await request.user.getShows();
   response.send(userShows);
 });
+
+userRouter.put("/:user_id/shows/:show_id", getUserById, async (request, response) => {
+  const userShows = await request.user.getShows();
+  const showToUpdate = userShows[request.params.show_id - 1];
+  showToUpdate.update(request.body);
+  response.status(201).send(showToUpdate);
+});
+
 
 module.exports = userRouter;
